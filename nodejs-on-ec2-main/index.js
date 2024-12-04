805875c939d1eb86492c5d8b47a83f8104aaa922
saif_ur_rahman <166166418+SirSaifUrRahman@users.noreply.github.com>
2024-12-05 04:31:47 +0500

    Update index.js

--- nodejs-on-ec2-main/index.js
+++ nodejs-on-ec2-main/index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Saif on fire 🔥'); //write a response to the client
+  res.write('Saif on fire'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 80
